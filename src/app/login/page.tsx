ac923290ce257bbc83dcc3d9524cd28d2c35b04a
Aneesh Ghosh <a1joyghosh@gmail.com>
2026-02-19 22:00:32 +0530

    added middleware and me route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,8 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axios, { Axios } from "axios";
-import toast from "react-hot-toast";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
   const router = useRouter();
